a20a778d69a72ddd68663ce60a815aa844439da7
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-07-11 14:55:43 -0300

    added express routing
    added skeletal express routes for endpoints to be used in movie API. will be added to in coming exercises.

--- index.js
+++ index.js
@@ -44,10 +44,52 @@ app.get('/', (req, res) => {
 
 app.use(express.static('public'));
 
+//return all movies
 app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
 
+//return info for specific title
+app.get('/movies/:title', (req, res) => {
+  res.send('successful GET response returning data of specific movie by title');
+});
+
+//return info for specific genre
+app.get('/movies/:title/:genre', (res, res) => {
+  res.send('successful GET response returning data about specific titles genre');
+});
+
+//return info on specific director
+app.get('/movies/:director', (req, res) => {
+  res.send('successful GET response returning data about a director');
+});
+
+//post data on new user
+app.post('/users/', (res, res) => {
+  res.send('a JSON holding data of the user that was added including username, password, email, date of birth');
+});
+
+//update user data
+app.put('/users/:username', (req, res) => {
+  res.send('a message saying their username has been updated');
+});
+
+//add movie to user's list of favorites
+app.put('/users/:favorites', (req, res) => {
+  res.send('a message saying a movie has been added top their list of favorites');
+});
+
+//remove movie from user's list of favorites
+app.delete('/users/:favorites', (req, res) => {
+  res.send('a message saying a movie has been removed from their list of favorites');
+});
+
+//user de-registration
+app.delete('/users', (req, res) => {
+  res.send('a message saying saying the users email has been removed');
+});
+
+//error message
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Oh No! What happened?!');
@@ -56,3 +98,4 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080');
 });
+
