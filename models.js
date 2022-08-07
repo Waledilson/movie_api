1308a397887ea46d08452f744927cadd6e962446
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-08-07 18:41:20 -0300

    Update models.js

--- models.js
+++ models.js
@@ -25,11 +25,11 @@ let userSchema = mongoose.Schema({
   });
   
   userSchema.statics.hashPassword = (password) => {
-    return bcrypt.hashSync(password, 10);
+    return bcryptjs.hashSync(password, 10);
   };
   
   userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.Password);
+    return bcryptjs.compareSync(password, this.Password);
   };
 
 let Movie = mongoose.model('Movie', movieSchema);
