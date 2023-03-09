53b22998ca59a7a072c9c40217c6e43aa2a55146
Waledilson <95579186+Waledilson@users.noreply.github.com>
2023-03-08 23:33:53 -0400

    yes backslash? no backslash?

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ let allowedOrigins = [
   "https://waledilson.github.io/myFlix-Angular-client/",
   "https://waledilson.github.io/movie_api-client/",
   "https://martini-shot-react.netlify.app/",
+  "https://martini-shot-react.netlify.app",
 ];
 app.use(
   cors({
